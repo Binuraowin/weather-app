502845927c22963551bb414436d5c2074a25304c
Binura owin <53115861+Binuraowin@users.noreply.github.com>
2021-03-15 20:49:41 +0530

    frontend

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-const request = require('request');request(url, function (err, response, body) {
+const request = require('request');
+const argv = require('yargs').argv;
+
+let apiKey = '3d9ff51f6e47a2e75456f5d1d24762df';
+let city = argv.c || 'portland';
+let deg = `https://api.openweathermap.org/data/2.5/weather?lat=43.700111&lon=-79.416298&appid=${apiKey}`;
+
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+
+request(deg, function (err, response, body) {
     if(err){
       console.log('error:', error);
     } else {
-      console.log('body:', body);
+      let weather = JSON.parse(body)
+      let message = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+      console.log(message);
+      console.log(weather);
     }
   });
